98d8fcfec5d999d98d1446854c4035de8e761487
Tyler Philbrick <t5j6p9@gmail.com>
2019-10-27 23:34:19 -0700

    Add new representation test

---
+++ test_repr.cc
@@ -0,0 +1,70 @@
+
+#include <cstdio>
+#include <cstdint>
+#include <iostream>
+#include <string>
+
+class [[gnu::packed]] ethernet {
+public:
+    int a, b;
+
+    ethernet() {
+        a = 0xaaaaaaaa;
+        b = 0xbbbbbbbb;
+    }
+};
+
+template <typename Encap>
+class [[gnu::packed]] ipv4 : public Encap {
+public:
+    int c, d;
+
+    ipv4() {
+        c = 0xcccccccc;
+        d = 0xdddddddd;
+    }
+};
+
+template <typename Encap>
+class [[gnu::packed]] ipv6 : public Encap {
+public:
+    int c[4], d[4];
+
+    ipv6() {
+        for (int i=0; i<4; i++) {
+            c[i] = 0x0c;
+            d[i] = 0x0d;
+        }
+    }
+};
+
+template <typename Encap>
+class [[gnu::packed]] udp : public Encap {
+public:
+    int e, f;
+
+    udp() {
+        e = 0xeeeeeeee;
+        f = 0xffffffff;
+    }
+};
+
+
+int dump(const char *ptr, int len) {
+    for (int i=0; i<len; i++) {
+        printf("%02hhx ", ptr[i]);
+        if (i % 16 == 7)  printf(" ");
+        if (i % 16 == 15)  printf("\n");
+    }
+    printf("\n");
+    return 0;
+}
+
+int main() {
+    udp<ipv4<ethernet>> i1{};
+    udp<ipv6<ethernet>> i6{};
+
+    dump(reinterpret_cast<const char *>(&i1), sizeof(i1));
+    dump(reinterpret_cast<const char *>(&i6), sizeof(i6));
+}
+
